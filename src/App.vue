3bea9955ac2bfd973f6d2eb36928a0f7d5e14120
Artem Okhotnikov <art.okhotnikov@yandex.ru>
2024-12-16 15:55:50 +0400

    fxi fxi fxi

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import ScreenshotProvider from './components/Loupe/ScreenshotProvider.vue';
+import ScreenshotProvider from './components/Loupe/ScreenshotProvider.vue'
 
 const route = useRoute()
 </script>
 
 <template>
   <div class="container">
+    <audio src="/audio.mp3" loop autoplay></audio>
     <ScreenshotProvider>
-    <RouterView v-slot="{ Component }">
-      <template v-if="Component">
-        <transition name="my" mode="out-in">
-          <suspense>
-            <component :is="Component" :key="route.fullPath"></component>
-          </suspense>
-        </transition>
-      </template>
-    </RouterView>
-  </ScreenshotProvider>
+      <RouterView v-slot="{ Component }">
+        <template v-if="Component">
+          <transition name="my" mode="out-in">
+            <suspense>
+              <component :is="Component" :key="route.fullPath"></component>
+            </suspense>
+          </transition>
+        </template>
+      </RouterView>
+    </ScreenshotProvider>
   </div>
 </template>
